ee1b24c3de75b8d1dfa44c785e0ef13fee9af82a
davidnene <davienesh4@gmail.com>
2023-07-06 10:40:21 +0300

    Basic route configurations

--- routes/api.php
+++ routes/api.php
@@ -14,6 +14,13 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+Route::get('/posts', function() {
+    response()->json([
+        'posts' => [
+            ['title' => 'post1']
+        ]
+        ]);
+});
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
